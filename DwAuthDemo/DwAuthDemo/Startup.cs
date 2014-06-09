586f6a8d83269a3ea24e6c648c7c944b1889ba03
s.rusinov <rsage@mail.ru>
2014-06-09 21:20:45 +0400

    MVC template

---
+++ DwAuthDemo/DwAuthDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DwAuthDemo.Startup))]
+namespace DwAuthDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
