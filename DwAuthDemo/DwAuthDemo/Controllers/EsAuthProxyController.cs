2293984104bd3995015e0abc8197fd4cb02acd63
s.rusinov <rsage@mail.ru>
2014-06-10 22:10:00 +0400

    EL requests interception.

---
+++ DwAuthDemo/DwAuthDemo/Controllers/EsAuthProxyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DwAuthDemo.Controllers
+{
+    [AllowAnonymous]
+    public class EsAuthProxyController : ApiController
+    {
+        public string Get(string id)
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ DwAuthDemo/DwAuthDemo/Controllers/EsProxyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using PlainElastic.Net;
+
+namespace DwAuthDemo.Controllers
+{
+    [AllowAnonymous]
+    public class EsProxyController : ApiController
+    {
+        [HttpGet]
+        [ActionName("_nodes")]
+        // GET: EsProxy/_nodes
+        public HttpResponseMessage GetNodes()
+        {
+            var conn = new ElasticConnection("localhost");
+            var res = conn.Get("_nodes");
+            var obj = JObject.Parse(res);
+            var response = Request.CreateResponse(HttpStatusCode.OK, obj, "application/json");
+            return response;
+        }
+
+        [HttpPost]
+        [ActionName("_all")]
+        // POST: EsProxy/_all
+        public async Task<HttpResponseMessage> PostQuery(string op)
+        {
+            var query = await Request.Content.ReadAsStringAsync();
+            var conn = new ElasticConnection("localhost");
+            var res = conn.Post("_all/" + op, query);
+            var obj = JObject.Parse(res);
+            var response = Request.CreateResponse(HttpStatusCode.OK, obj, "application/json");
+            return response;
+        }
+    }
+}
